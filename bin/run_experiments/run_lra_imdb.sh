fa4af08cd252d998a2c8a825c5e094978aac1e31
Jimmy Smith <jimmysmith1919@gmail.com>
2022-12-10 12:16:55 -0800

    modify imdb config file for liquid approach

--- bin/run_experiments/run_lra_imdb.sh
+++ bin/run_experiments/run_lra_imdb.sh
@@ -1,6 +1,6 @@
 python run_train.py --C_init=lecun_normal --activation_fn=half_glu2 \
-                    --batchnorm=True --bidirectional=True --blocks=12 --bsz=50 \
-                    --d_model=256 --dataset=imdb-classification \
-                    --dt_global=True --epochs=35 --jax_seed=8825365 --lr_factor=4 \
-                    --n_layers=6 --opt_config=standard --p_dropout=0.1 --ssm_lr_base=0.001 \
-                    --ssm_size_base=192 --warmup_end=0 --weight_decay=0.07
+                    --batchnorm=True --bidirectional=False --blocks=1 --bsz=50 \
+                    --d_model=128 --dataset=imdb-classification \
+                    --dt_global=False --epochs=50 --jax_seed=8825365 --lr_factor=3 \
+                    --n_layers=4 --opt_config=standard --p_dropout=0.1 --ssm_lr_base=0.001 \
+                    --ssm_size_base=8 --warmup_end=0 --weight_decay=0.01 --liquid=True
